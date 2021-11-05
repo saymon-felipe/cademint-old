f4f663887d664ac5bf776b75ea21e598b70e48d3
saymon-felipe <linnubr@gmail.com>
2021-11-04 23:14:28 -0300

    Versionamento

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ function changeAppVersionAndUrl(ambient, version) {
 // A centena refere-se à alterações grandes na usabilidade e no conceito em geral.
 //
 // ==============================
-   changeAppVersionAndUrl(1, "0.0.3");
+   changeAppVersionAndUrl(1, "0.0.4");
 // ==============================
 
 //Início da execução.
